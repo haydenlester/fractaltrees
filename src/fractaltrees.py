d61e980aa8f7ee27e934ba6b671b2ecdeb8ebb45
hcldev <hcldev@protonmail.com>
2021-04-22 01:15:29 -0500

    Minor formatting changes

--- src/fractaltrees.py
+++ src/fractaltrees.py
@@ -1,7 +1,6 @@
 import pygame
 import math
 
-
 # Variables
 (width, height) = (900, 900)
 background_color = pygame.Color(47, 143, 148)
@@ -19,6 +18,7 @@ screen.fill(background_color)
 
 
 # Classes
+# Class branch represents a branch of the tree
 class Branch:
 
     def __init__(self, x, y, ang, length):
@@ -33,6 +33,7 @@ class Branch:
     def display(self):
         pygame.draw.line(screen, branch_color, (self.x, self.y), (self.endx, self.endy))
 
+
 # Recursive draw function
 def drawtree(prevBrEndX, prevBrEndY, prevBrAng, prevBrLen, remDepth):
     if remDepth > 0:
@@ -42,8 +43,8 @@ def drawtree(prevBrEndX, prevBrEndY, prevBrAng, prevBrLen, remDepth):
         br2.display()
         drawtree(br1.endx, br1.endy, br1.ang, br1.length, remDepth - 1)
         drawtree(br2.endx, br2.endy, br2.ang, br2.length, remDepth - 1)
-        del br1
-        del br2
+        del br1  # Mem cleanup, going to add sliders so don't want these objects clogging stuff up
+        del br2  # Probably will just remove the class altogether
 
 
 def main():
@@ -56,12 +57,12 @@ def main():
     # Update the display
     pygame.display.flip()
 
+    # Termination conditions
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-
 
 pygame.display.flip()
 
